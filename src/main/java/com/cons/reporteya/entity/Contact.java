e256303c07cad3595587246ea448c0683fb52914
Christian Castellon <138260817+castellonchris432@users.noreply.github.com>
2023-09-06 16:43:38 -0300

    ultimo
    hola

--- src/main/java/com/cons/reporteya/entity/Contact.java
+++ src/main/java/com/cons/reporteya/entity/Contact.java
@@ -1,13 +1,15 @@
 package com.cons.reporteya.entity;
 
 import java.util.Date;
-import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
@@ -39,6 +41,10 @@ public class Contact {
 
 	private Date updated_at;
 
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
+	
 	
 	
 	@PrePersist
---
+++ src/main/java/com/cons/reporteya/repository/ContactRepository.java
@@ -0,0 +1,14 @@
+package com.cons.reporteya.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cons.reporteya.entity.Contact;
+
+@Repository
+public interface ContactRepository extends CrudRepository<Contact, Long>{
+	
+	
+
+	
+}
