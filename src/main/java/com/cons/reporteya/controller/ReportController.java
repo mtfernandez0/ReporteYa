3ae5aea1d56d3faa483dffbd5c20611907c1d828
IgnacioSenicen <senicenmuoz@gmail.com>
2023-09-06 17:20:21 -0300

    Subo la vista de reportes

--- src/main/java/com/cons/reporteya/controller/ReportController.java
+++ src/main/java/com/cons/reporteya/controller/ReportController.java
@@ -1,21 +1,27 @@
 package com.cons.reporteya.controller;
 
-import com.cons.reporteya.entity.Marker;
-import com.cons.reporteya.entity.Report;
-import com.cons.reporteya.service.ReportService;
+import java.security.Principal;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.cons.reporteya.entity.Marker;
+import com.cons.reporteya.entity.Report;
+import com.cons.reporteya.service.ReportService;
+import com.cons.reporteya.service.UserService;
 
 @Controller
 @RequestMapping("/reports")
 public class ReportController {
 
     private final ReportService reportService;
-
-    public ReportController(ReportService reportService) {
+    private final UserService userService;
+    public ReportController(ReportService reportService, UserService userService) {
         this.reportService = reportService;
+        this.userService = userService;
     }
 
     @GetMapping("/new")
@@ -23,4 +29,12 @@ public class ReportController {
                             @ModelAttribute("report") Report report){
         return "report/new";
     }
+    
+    @GetMapping("")
+    public String reports (Model model, Principal principal ) {
+    	model.addAttribute("reports",reportService.findAll());
+    	model.addAttribute("user",userService.findByEmail(principal.getName()));
+    	return "report/reports";
+    }
+    
 }
