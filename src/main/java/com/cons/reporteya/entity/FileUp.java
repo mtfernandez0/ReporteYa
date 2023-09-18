48ad0e35ae4437e3925cfd9e56b730c2ee4a8306
Matias <78266637+mtfernandez0@users.noreply.github.com>
2023-09-17 22:56:45 -0300

    Added image functionality

--- src/main/java/com/cons/reporteya/entity/FileUp.java
+++ src/main/java/com/cons/reporteya/entity/FileUp.java
@@ -13,41 +13,39 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 
 @Entity
-@Table(name="archivos")
-@Data
+@Table(name = "archivos")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 @Builder
 public class FileUp {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	@Column(updatable = false)
-	private Date createdAt;
-	private Date updatedAt;
-	
-	private String rutaArchivo;
-	private String nombre;
-	private String fileType;
-	
-	 @ManyToOne(fetch = FetchType.LAZY)
-	    @JoinColumn(name="report_id")
-	    private Report reporte;
-	
-	@PrePersist
-	protected void onCreate() {
-		this.createdAt = new Date();
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@PreUpdate
-	protected void onUpdate() {
-		this.updatedAt = new Date();
-	}
-	
-	
-	
+    @Column(updatable = false)
+    private Date createdAt;
+    private Date updatedAt;
 
+    private String rutaArchivo;
+    private String nombre;
+    private String fileType;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "report_id")
+    private Report reporte;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = new Date();
+    }
 }
